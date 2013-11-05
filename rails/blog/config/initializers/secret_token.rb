5c27facc4035d515dbb9de868a973e5d52fc2b13
William Falk-Wallace <wfalkwallace@gmail.com>
2013-11-05 18:36:41 -0500

    updates for gitignore stuff

--- rails/blog/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = '7b16c9caefb48d40ea254f02e9ea57459c98435b3bbb993d44171142f8564a8c2d77b356b7ca5027074e9e0adab6344ca198c63209872e5b0e05f433628a9ce6'
