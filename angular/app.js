6f1c56022587812704ddaffa2f84195cb4ea8cab
William Falk-Wallace <wgf2104@columbia.edu>
2014-12-02 18:30:07 -0800

    too many assets to pull down - doing it in browser

--- angular/app.js
+++ angular/app.js
@@ -8,15 +8,30 @@
   var gems = [
     {
       name: 'Dodecahedron',
-      price: 2.95,
+      price: 2,
       description: '. . .',
-      canPurchase: true,
+      images: [
+        {
+          full: 'dodecahedrom-01-full.jpg',
+          thumb: 'dodecahedrom-01-thumb.jpg'
+        },
+        {
+          full: 'dodecahedrom-02-full.jpg',
+          thumb: 'dodecahedrom-02-thumb.jpg'
+        }
+      ]
     },
     {
       name: 'Pentagonal Gem',
       price: 5.95,
       description: '. . .',
       canPurchase: false,
+      images: [
+        {
+          full: 'pentagonal-01-full.jpg',
+          thumb: 'pentagonal-01-thumb.jpg'
+        }
+      ]
     }
   ];
 
