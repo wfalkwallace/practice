c01f6567a0e6e77e60e889baeec7e2bfb964e42b
William Falk-Wallace <wgf2104@columbia.edu>
2015-01-25 13:11:37 -0800

    adds elevator game solution through level 5

---
+++ javascript/elevator.js
@@ -0,0 +1,48 @@
+{
+    init: function(elevators, floors) {
+        var inPath = function(elevator, floor) {
+            return (elevator.currentFloor() > floor
+                    && elevator.goingDownIndicator()
+                    || elevator.currentFloor() < floor
+                    && elevator.goingUpIndicator())
+        };
+        var isFull = function(elevator) {
+            return elevator.loadFactor() === 1;
+        };
+        var elevator = function(floor, direction) {
+            for (var i in elevators) {
+                var el = elevators[i];
+                var dir = el.goingUpIndicator() ? 'up' : 'down';
+                if (!isFull(el)
+                    && inPath(el, floor)
+                    && (dir == direction || el.destinationQueue.length === 0)) {
+                    return i;
+                }
+            }
+            return Math.floor(Math.random() * elevators.length);
+        };
+
+        for (var i in floors) {
+            floors[i].on("up_button_pressed", function() {
+                elevators[elevator(this.floorNum(), 'up')].goToFloor(this.floorNum());
+            });
+            floors[i].on("down_button_pressed", function(thing) {
+                elevators[elevator(this.floorNum(), 'down')].goToFloor(this.floorNum());
+            });
+        }
+        for (var i in elevators) {
+            elevators[i].on("floor_button_pressed", function(floorNum) {
+                this.goToFloor(floorNum);
+            });
+        }
+        // for (var i in elevators) {
+        //     elevators[i].on("idle", function() {
+        //         // this.goToFloor(Math.floor(Math.random() * floors.length));
+        //         this.goToFloor(0);
+        //     });
+        // }
+    },
+    update: function(dt, elevators, floors) {
+        // We normally don't need to do anything here
+    }
+}
